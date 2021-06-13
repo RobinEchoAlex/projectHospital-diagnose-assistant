faf142e57653d9ccd0da49a330f0e6329e7427c5
RobinEchoAlex <872576923@qq.com>
2021-06-13 13:41:41 +0100

    Distinct Symptoms can be identified in code

--- DiagnoseAssistant/DiagnoseAssistant/DiagnoseAssistantLibrary/Patches/DiagnosesTable.cs
+++ DiagnoseAssistant/DiagnoseAssistant/DiagnoseAssistantLibrary/Patches/DiagnosesTable.cs
@@ -1,7 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using HarmonyLib;
 using Lopital;
-
+using UnityEngine;
 using UnityEngine.UI;
+using Object = System.Object;
 
 namespace DiagnoseAssistant.Patches
 {
@@ -10,18 +14,40 @@ namespace DiagnoseAssistant.Patches
     public static class DiagnosesTable_FillData
     {
         public static void Postfix(DiagnosesTable __instance)
-        { 
+        {
             UnityEngine.Debug.LogError("postfixrun");
-            
+
+            Dictionary<GameDBSymptom, int> symptomTimes = new Dictionary<GameDBSymptom, int>();
             // TableController component = __instance.GetComponent<TableController>();
             foreach (PossibleDiagnosis possibleDiagnosis in __instance.m_diagnosesToSort)
             {
-                UnityEngine.Debug.LogError(StringTable.GetInstance().GetLocalizedText((DatabaseEntry) possibleDiagnosis.m_diagnosis.Entry));
+                GameDBMedicalCondition entry = possibleDiagnosis.m_diagnosis.Entry;
+                UnityEngine.Debug.Log(StringTable.GetInstance().GetLocalizedText((DatabaseEntry) entry));
+                foreach (var symptom in entry.Symptoms)
+                {
+                    var symptomRef = symptom.GameDBSymptomRef.Entry;
+                    if (!symptomTimes.ContainsKey(symptomRef))
+                    {
+                        symptomTimes.Add(symptomRef, 1);
+                    }
+                    else
+                    {
+                        symptomTimes [symptomRef] = symptomTimes[symptomRef] + 1;
+                    }
+                }
+            }
+
+            var distinctSymptomDict = symptomTimes.Where(s => s.Value == 1)
+                .ToDictionary(s => s.Key, s => s.Value);
+
+            foreach (var distinctSymptom in distinctSymptomDict)
+            {
+                UnityEngine.Debug.Log("distinct Symptom:" +
+                                      StringTable.GetInstance().GetLocalizedText(distinctSymptom.Key));
             }
 
             // ((Graphic) component.GetItem(1, 1).m_gameObject.GetComponentInChildren<Image>())
             //     .color = UISettings.Instance.SYMPTOM_COLOR_HAZARD_HIGH;
-            
         }
     }
 }
